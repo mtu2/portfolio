02c4e1cc482bf131b7f7aade02de6ba44456c69a
Michael <michael52423@gmail.com>
2021-02-24 21:51:29 +1100

    Added other projects section

--- src/App.js
+++ src/App.js
@@ -5,17 +5,64 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 library.add(fab, fas);
 
 function App() {
+  const [expandOtherProjects, setExpandOtherProjects] = useState(false);
+  const otherProjectsRef = useRef(null);
+
   const aboutMeRef = useRef(null);
   const projectsRef = useRef(null);
   const reachOutRef = useRef(null);
 
   function scrollIntoView(ref) {
     ref.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function handleExpandOtherProjects() {
+    if (expandOtherProjects) collapseSection(otherProjectsRef.current);
+    else expandSection(otherProjectsRef.current);
+
+    setExpandOtherProjects(!expandOtherProjects);
+  }
+
+  function collapseSection(element) {
+    // get height of the element's inner content
+    const sectionHeight = element.scrollHeight;
+
+    // temporarily disable all css transitions
+    const elementTransition = element.style.transition;
+    element.style.transition = "";
+
+    // on the next frame, set element's height to current pixel height
+    requestAnimationFrame(() => {
+      element.style.height = `${sectionHeight}px`;
+      element.style.transition = elementTransition;
+
+      // on the next frame have the element transition to height: 0
+      requestAnimationFrame(() => {
+        element.style.height = 0;
+      });
+    });
+  }
+
+  function expandSection(element) {
+    // get the height of the element's inner content
+    const sectionHeight = element.scrollHeight;
+
+    // have the element transition to the height of its inner content
+    element.style.height = sectionHeight + "px";
+
+    // when the next css transition finishes (one we just set off)
+    element.addEventListener("transitionend", function callback(e) {
+      // remove this event listener so it only gets triggered once
+      element.removeEventListener("transitionend", callback);
+
+      // set height to auto
+      element.style.height = "auto";
+    });
   }
 
   return (
@@ -105,13 +152,13 @@ function App() {
           <div className={styles.projectTitle}>
             <h3>Pomotimer</h3>
             <div>
-              <a href="http://mtu-pomodoro.herokuapp.com/" title="Pomodoro">
+              <a href="https://pomotimer.herokuapp.com/" title="Pomotimer">
                 <FontAwesomeIcon
                   icon={["fas", "external-link-alt"]}
                   className={styles.icon}
                 />
               </a>
-              <a href="https://github.com/mtu2/pomodoro" title="GitHub">
+              <a href="https://github.com/mtu2/pomotimer" title="GitHub">
                 <FontAwesomeIcon
                   icon={["fab", "github"]}
                   className={styles.icon}
@@ -193,42 +240,53 @@ function App() {
           <code>jQuery, Chrome API, Stack Exchange API, SASS</code>
         </div>
 
-        <div className={styles.project}>
-          <div className={styles.projectTitle}>
-            <h3>Other projects...</h3>
+        <div className={styles.otherProjectsContainer}>
+          <div
+            className={styles.expandOtherProjects}
+            onClick={handleExpandOtherProjects}
+          >
+            <p>Other projects...</p>
+            <FontAwesomeIcon
+              icon={["fas", "chevron-down"]}
+              className={`${styles.icon} ${
+                expandOtherProjects && styles.rotate
+              }`}
+            />
           </div>
-        </div>
 
-        {/* <div className={styles.project}>
-          <div className={styles.projectTitle}>
-            <h3>Simple Definition</h3>
-            <div>
-              <a
-                href="https://simple-definition.mttu.dev/"
-                title="Simple Definition"
-              >
-                <FontAwesomeIcon
-                  icon={["fas", "external-link-alt"]}
-                  className={styles.icon}
-                />
-              </a>
-              <a
-                href="https://github.com/mtu2/simple-definition"
-                title="GitHub"
-              >
-                <FontAwesomeIcon
-                  icon={["fab", "github"]}
-                  className={styles.icon}
-                />
-              </a>
+          <div ref={otherProjectsRef} className={styles.otherProjects}>
+            <div className={styles.project}>
+              <div className={styles.projectTitle}>
+                <h3>Simple Definition</h3>
+                <div>
+                  <a
+                    href="https://simple-definition.mttu.dev/"
+                    title="Simple Definition"
+                  >
+                    <FontAwesomeIcon
+                      icon={["fas", "external-link-alt"]}
+                      className={styles.icon}
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/mtu2/simple-definition"
+                    title="GitHub"
+                  >
+                    <FontAwesomeIcon
+                      icon={["fab", "github"]}
+                      className={styles.icon}
+                    />
+                  </a>
+                </div>
+              </div>
+              <p>
+                A simple web app that generates random words with definitions
+                and examples.
+              </p>
+              <code>HTML/CSS, React</code>
             </div>
           </div>
-          <p>
-            A simple web app that generates random words with definitions and
-            examples.
-          </p>
-          <code>HTML/CSS, React</code>
-        </div> */}
+        </div>
       </div>
 
       {/* Reach Out */}
@@ -247,7 +305,7 @@ function App() {
       {/* Footer */}
       <footer className={styles.container}>
         <p>
-          <strong>©️ {new Date().getFullYear()}, Michael Tu 💖</strong>
+          <strong>©️ {new Date().getFullYear()}, Michael Tu</strong>
         </p>
         <div className={styles.footerIcons}>
           <a href="https://github.com/mtu2" title="GitHub">
